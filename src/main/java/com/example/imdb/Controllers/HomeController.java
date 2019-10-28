7c0e45035fa2dcefc253c9427703ce5ed0fb59fd
Jonathan <jonathaneisbenzon@gmail.com>
2019-10-28 05:07:19 +0100

    moved a bunch of files to new locations

--- src/main/java/com/example/imdb/Controllers/HomeController.java
+++ src/main/java/com/example/imdb/Controllers/HomeController.java
@@ -24,73 +24,28 @@ public class HomeController {
     public String index(Model model, @RequestParam(defaultValue = "") String search) {
         List<MovieRatings> movies = movieService.MovieRatingListSearch(search);
         model.addAttribute("movies", movies);
-        return "IMDB";
-    }
-
-    @GetMapping("/admin")
-    public String admin(Model model, @RequestParam(defaultValue = "") String search) {
-        List<MovieRatings> movies = movieService.MovieRatingListSearch(search);
-        model.addAttribute("movies", movies);
-        return "admin";
+        return "views/IMDB";
     }
 
     @GetMapping("/details/{id}")
     public String details(@PathVariable int id, Model model) {
 
         model.addAttribute("movies", movieService.SelectMovie(id));
-        return "details";
-    }
-
-    @GetMapping("/create")
-    public String Create(Model model) {
-        model.addAttribute("movies", new Movies());
-        return "create";
-    }
-
-    @PostMapping("/create")
-    public String create(@ModelAttribute Movies movies) {
-        movieService.CreateMovie(movies);
-        return "redirect:/admin";
+        return "views/details";
     }
 
     @GetMapping("rate/{id}")
     public String rate(@PathVariable int id, Model model) {
+        //ratingService.CreaateDefaultRating(id);
         model.addAttribute("rating", ratingService.SelectRatingByRatingIDwhenRating(id));
-        return "Rate";
+        return "views/rate";
     }
 
     @PostMapping("/rate")
     public String rate(@ModelAttribute Ratings ratings) {
         ratingService.CreateRating(ratings);
-
         return "redirect:/";
     }
-
-    @GetMapping("edit/{id}")
-    public String edit(@PathVariable int id, Model model) {
-        model.addAttribute("movies", movieService.SelectMovie(id));
-        return "edit";
-    }
-
-    @PostMapping("/edit")
-    public String edit(@ModelAttribute Movies movies, Model model){
-        movieService.UpdateMovie(movies);
-        model.addAttribute("movies", movieService.MovieRatingList());
-        return "redirect:/admin";
-    }
-
-    @GetMapping("/delete/{id}")
-    public String delete(@PathVariable int id, Model model) {
-        model.addAttribute("movies", movieService.SelectMovie(id));
-        return "delete";
-    }
-
-    @PostMapping("/delete")
-    public String delete(@ModelAttribute Movies movies) {
-        movieService.DeleteMovie(movies.getId());
-        return "redirect:/admin";
-    }
-
 
 
 }
